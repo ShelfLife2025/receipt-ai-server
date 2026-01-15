03d971ec68b1086c937ca17ec013805a7ecca3c2
ShelfLife2025 <bryce.rmiller@icloud.com>
2026-01-15 15:31:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -777,6 +777,7 @@ ABBREV_TOKEN_MAP: dict[str, str] = {
     "pub": "publix",
     "pbl": "publix",
     "pbx": "publix",
+    "gg": "good and gather"
     "chees": "cheese",
 
     # common brand/product abbreviations
